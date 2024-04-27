ca21c646ffa34641518fac64c129c026c7ee1c0f
dalew75 <dalew75@gmail.com>
2024-04-27 10:24:03 -0400

    allow command line arg usage, increase slippage, more logging

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 import RaydiumSwap from './RaydiumSwap'
 import { Transaction, VersionedTransaction, LAMPORTS_PER_SOL } from '@solana/web3.js'
 
-const swap = async () => {
-  const executeSwap = false // Change to true to execute swap
+const [quoteMint, tokenAAmount] = process.argv.slice(2);
+
+const swap = async (tokenMint: string, tokenAAmount: number) => {
+  const executeSwap = true // Change to true to execute swap
   const useVersionedTransaction = true // Use versioned transaction
-  const tokenAAmount = 0.01 // e.g. 0.01 SOL -> B_TOKEN
 
   const baseMint = 'So11111111111111111111111111111111111111112' // e.g. SOLANA mint address
-  const quoteMint = 'HZ1JovNiVvGrGNiiYvEozEVgZ58xaU3RKwX8eACQBCt3' // e.g. PYTH mint address
 
-  const raydiumSwap = new RaydiumSwap(process.env.RPC_URL, process.env.WALLET_PRIVATE_KEY)
-  console.log(`Raydium swap initialized`)
+  const raydiumSwap = new RaydiumSwap(process.env.RPC_URL, process.env.WALLET_PRIVATE_KEY);
+  const publicKey = raydiumSwap.wallet.publicKey.toBase58();
+  console.log(`Raydium swap initialized, executeSwap=${executeSwap}, using RPC URL: ${process.env.RPC_URL} and wallet: ${publicKey}`);
 
   // Loading with pool keys from https://api.raydium.io/v2/sdk/liquidity/mainnet.json
   await raydiumSwap.loadPoolKeys()
-  console.log(`Loaded pool keys`)
+  const startTime = Date.now();
+  await raydiumSwap.loadPoolKeys();
+  const endTime = Date.now();
+  console.log(`Loaded pool keys in ${endTime - startTime} milliseconds`);
 
   // Trying to find pool info in the json we loaded earlier and by comparing baseMint and tokenBAddress
   let poolInfo = raydiumSwap.findPoolInfoForTokens(baseMint, quoteMint)
@@ -34,7 +38,7 @@ const swap = async () => {
     0.0005 * LAMPORTS_PER_SOL, // Prioritization fee, now set to (0.0005 SOL)
     useVersionedTransaction,
     'in',
-    5 // Slippage
+    15 // Slippage
   )
 
   if (executeSwap) {
@@ -48,8 +52,8 @@ const swap = async () => {
       ? await raydiumSwap.simulateVersionedTransaction(tx as VersionedTransaction)
       : await raydiumSwap.simulateLegacyTransaction(tx as Transaction)
 
-    console.log(simRes)
+    console.log('Simulated transaction', simRes);
   }
 }
 
-swap()
+swap(quoteMint, parseFloat(tokenAAmount));
