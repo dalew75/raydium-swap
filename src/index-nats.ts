0408da7956e1dba7144ce8f13137efdb3ed63b38
dalew75 <dalew75@gmail.com>
2024-05-19 11:52:23 -0400

    checkpoint on index-nats including get pool info

--- src/index-nats.ts
+++ src/index-nats.ts
@@ -1,46 +1,50 @@
+import 'dotenv/config';
 import RaydiumSwap from './RaydiumSwap'
 import { Transaction, VersionedTransaction, LAMPORTS_PER_SOL } from '@solana/web3.js'
-import { connect, NatsConnection, StringCodec } from 'nats';
+import { subscribe, initNats } from './nats';
+import { getPoolID } from './pools';
+import { Commitment, PublicKey, Connection, AccountInfo } from '@solana/web3.js';
+import { LIQUIDITY_STATE_LAYOUT_V4 } from "@raydium-io/raydium-sdk";
 
 let [quoteMint, tokenAAmountString] = process.argv.slice(2);
 
-const sc = StringCodec();
-let nc: NatsConnection;
-
 let raydiumSwap: RaydiumSwap;
+
+let poolKeys: any;
 
 async function loadRaydium() {
   raydiumSwap = new RaydiumSwap(process.env.RPC_URL, process.env.WALLET_PRIVATE_KEY);
   const publicKey = raydiumSwap.wallet.publicKey.toBase58();
 
-  // Loading with pool keys from https://api.raydium.io/v2/sdk/liquidity/mainnet.json
-  await raydiumSwap.loadPoolKeys()
   const startTime = Date.now();
+
+  // Loading with pool keys from https://api.raydium.io/v2/sdk/liquidity/mainnet.json
+  poolKeys = await raydiumSwap.loadPoolKeys()
+
   const endTime = Date.now();
-  console.log(`Loaded pool keys in ${endTime - startTime} milliseconds`);
+  console.log(`Loaded ${poolKeys.length} pool keys in ${endTime - startTime} milliseconds`);
 
   console.log(`Raydium swap initialized, using RPC URL: ${process.env.RPC_URL} and wallet: ${publicKey}`);
 }
 
 async function initNatsListener(): Promise<void> {
-  nc = await connect({ servers: 'nats://167.172.56.2:4222' });
-  console.log("connected to nats");
+  await initNats(true);
+  subscribe("hello", (msg) => {
+    console.log("Received message: ", msg);
+    const parsedData = JSON.parse(msg);
+    console.log(`incoming msg on hello: `, parsedData);
+    swap(parsedData.quoteMint, parsedData.baseMint, parseFloat('0.01'));
+  });
 
-  const sub = nc.subscribe("hello");
-  (async () => {
-    for await (const m of sub) {
-      const decodedData = sc.decode(m.data);
-      try {
-        const parsedData = JSON.parse(decodedData);
-        console.log(`[${sub.getProcessed()}]:`, parsedData);
-        // swap(parsedData.quoteMint,parsedData.baseMint,parsedData.amount);
-        swap(parsedData.quoteMint, parsedData.baseMint, parseFloat('0.01'));
-      } catch (error) {
-        console.log(`[${sub.getProcessed()}]: ${decodedData}`);
-      }
-    }
-    console.log("subscription closed");
-  })();
+  subscribe("priceCheck", (msg) => {
+    console.log("Received message: ", msg);
+    const parsedData = JSON.parse(msg);
+    console.log(`incoming msg on priceCheck: `, parsedData);
+    console.log(poolKeys);
+    // const poolInfo = await Liquidity.fetchInfo({ connection: this.connection, poolKeys })
+    // const price = Liquidity.getRate(poolInfo)
+    // console.log(poolKeys);
+  });
 }
 
 
@@ -50,11 +54,12 @@ const swap = async (tokenMint: string, baseMint: string, tokenAAmount: number) =
   const useVersionedTransaction = true // Use versioned transaction
 
   const publicKey = raydiumSwap.wallet.publicKey.toBase58();
+  console.log(`Raydium swap initialized, executeSwap=${executeSwap}, using RPC URL: ${process.env.RPC_URL} and wallet: ${publicKey}`);
 
   // Trying to find pool info in the json we loaded earlier and by comparing baseMint and tokenBAddress
-  let poolInfo = raydiumSwap.findPoolInfoForTokens(baseMint, quoteMint)
+  let poolInfo = raydiumSwap.findPoolInfoForTokens(baseMint, tokenMint)
 
-  if (!poolInfo) poolInfo = await raydiumSwap.findRaydiumPoolInfo(baseMint, quoteMint)
+  if (!poolInfo) poolInfo = await raydiumSwap.findRaydiumPoolInfo(baseMint, tokenMint)
 
   if (!poolInfo) {
     throw new Error("Couldn't find the pool info");
@@ -80,7 +85,7 @@ const swap = async (tokenMint: string, baseMint: string, tokenAAmount: number) =
         : await raydiumSwap.sendLegacyTransaction(tx as Transaction)
 
       console.log(`EXECUTED SWAP! https://solscan.io/tx/${txid}`);
-      console.log(`EXECUTED SWAP! https://dexscreener.com/solana/${tokenMint}`);
+      console.log(`EXECUTED SWAP! https://dexscreener.com/solana/${tokenMint}?maker=${publicKey}`);
     } catch (error) {
       console.error('Error executing swap:', error);
       console.log('Retrying in 2 seconds...');
@@ -98,13 +103,29 @@ const swap = async (tokenMint: string, baseMint: string, tokenAAmount: number) =
   }
 }
 
-loadRaydium();
-if (quoteMint && tokenAAmountString) {
-  swap(quoteMint, 'So11111111111111111111111111111111111111112', parseFloat(tokenAAmountString));
+(async () => {
+  //loadRaydium();
+  if (quoteMint && tokenAAmountString) {
+    const connection = new Connection(process.env.HTTP_URL || '', {
+      wsEndpoint: process.env.WSS_URL || '',
+      // below is causing get transaction to fail
+      // httpHeaders: { "x-session-hash": SESSION_HASH }
+    });
+    const poolId = await getPoolID(quoteMint, connection);
+    console.log(`Pool ID: ${poolId}`);
+    const accountInfo = await connection.getAccountInfo(new PublicKey(quoteMint));  
+    console.log(`Account Info:`,accountInfo);
+    if (accountInfo && accountInfo.data.length > 0) {
+      const decodedData = LIQUIDITY_STATE_LAYOUT_V4.decode(Buffer.from(accountInfo.data));
+      console.log('Decoded account data:', decodedData);
+      console.log('Decoded account data baseMint:', decodedData.baseMint.toString());
+      // Use the decoded data as needed
+    }
 
-}
-else {
-  initNatsListener();
-}
+    //swap(quoteMint, 'So11111111111111111111111111111111111111112', parseFloat(tokenAAmountString));
 
-//swap(quoteMint, 'So11111111111111111111111111111111111111112', parseFloat(tokenAAmountString));
+  }
+  else {
+    initNatsListener();
+  }
+})();
